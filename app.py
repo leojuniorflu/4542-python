5f2d2a4e7d550857c87894284579204cce2e12ec
unknown <leojuniorflu@yahoo.com.br>
2022-10-10 22:33:35 -0300

    FEAT: Adicioando Comentario

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ APP_HOST  = os.environ['APP_HOST'] if 'APP_HOST' in os.environ else '127.0.0.1'
 APP_PORT  = os.environ['APP_PORT'] if 'APP_PORT' in os.environ else 5000
 APP_DEBUG = bool(os.environ['APP_DEBUG']) if 'APP_DEBUG' in os.environ else False
 
+//teste
+
 app = Flask(__name__)
 
 @app.route('/')
